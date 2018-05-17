b8a93c2b7ebd4726ad52b7bc8c5dac511ca0b9ef
Luuk3333 <luukbolding@live.nl>
2018-05-17 21:54:52 +0200

    Add command-line options to decoder code

--- image-to-file.py
+++ image-to-file.py
@@ -1,23 +1,31 @@
 from PIL import Image
 import array
+import argparse
 
-filename = 'myfile.txt._penc_.png'
+# Set command-line options
+parser = argparse.ArgumentParser()
+parser.add_argument('-F', '--file', help='Select file to decode.', dest="file")
+parser.add_argument('-V', '--verbose', help='Turn verbose mode on/off. Note: Performance may be degraded if this option is used.', dest="verbose", action="store_true", default=False)
+args = parser.parse_args()
+
+file = args.file
+verbose = args.verbose
 
 # Open image
 print("Opening image..")
-image = Image.open(filename)
+image = Image.open(file)
 
 # Remove '._penc_.png' to get original filename
-original_filename = filename.split('._penc_.png')[0]
+original_filename = file.split('._penc_.png')[0]
 
 # Get decimal pixel values and convert it to a bytearray
 print("Converting pixels to bytes..")
 pixels = bytes(image.getdata())
 
-#print(pixels)
+if verbose: print(pixels) # Show bytes if verbose is set to true
 
 # Save file
 print("Saving file..")
-open("_decoded_" + original_filename, 'wb').write(pixels)
+open(original_filename, 'wb').write(pixels)
 
 print("Finished!")
